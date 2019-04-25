f1054934959d6eee43d56a80ebd1157df31348f1
sukandawiguna <sukandawiguna@mail.unpas.ac.id>
2019-04-25 13:23:59 +0700

    first commit

---
+++ Coba_Xamarin/Coba_Xamarin/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Coba_Xamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            valueLabel.Text = ((Slider)sender).Value.ToString("F3");
+        }
+
+        async void OnButtonClicked(Object sender, EventArgs args)
+        {
+            Button button = (Button)sender;
+            await DisplayAlert("Clicked", " The button labelid'" + button.Text + "'has been clicked '", "OK");
+        }
+    }
+
+   
+}
